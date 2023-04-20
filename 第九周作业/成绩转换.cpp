20b6a06c57be6f10b43f13280a8367e60300edc4
钟艺波 <113235913+blameitonme1@users.noreply.github.com>
2023-04-20 10:24:56 +0800

    Create 成绩转换.cpp

---
+++ 第九周作业/成绩转换.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x>=90)cout<<"A"<<endl;
+    else if(x>=80&&x<90)cout<<"B"<<endl;
+    else if(x>=70&&x<80)cout<<"C"<<endl;
+    else if(x>=60&&x<70)cout<<"D"<<endl;
+    else if(x<60)cout<<"E"<<endl;;
+    getchar();
+    getchar();
+
+    return 0;
+}

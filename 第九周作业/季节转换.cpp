3a090e02dd4962921dffd9055d5127c07bbf8ba6
钟艺波 <113235913+blameitonme1@users.noreply.github.com>
2023-04-20 10:16:54 +0800

    Create 季节转换.cpp

---
+++ 第九周作业/季节转换.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    switch(x)
+    {
+        case 1:
+        {
+            cout<<"Spring"<<endl;
+            break;
+        }
+        case 2:
+        {
+            cout<<"Summer"<<endl;
+            break;
+        }
+        case 3:
+        {
+            cout<<"Fall"<<endl;
+            break;
+        }
+        case 4:
+        {
+            cout<<"Winter"<<endl;
+            break;
+        }
+    }
+    getchar();
+    getchar();
+
+    return 0;
+}

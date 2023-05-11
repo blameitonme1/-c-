7ffc2fa1f3d7dff3600081156275c93cb3c12dc0
钟艺波 <113235913+blameitonme1@users.noreply.github.com>
2023-05-11 10:20:46 +0800

    Update 单词长度.cpp

--- 第十二周作业.cpp/单词长度.cpp
+++ 第十二周作业.cpp/单词长度.cpp
@@ -1,1 +1,24 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string res;
+    char res_[500];
+    cin.getline(res_,500);
+    res=res_;
+    int len=0;
+    for(int i=0;i<res.size();i++)
+    {
+        if(res[i]!=' ')
+        len++;
+        else
+        {
+            cout<<len<<' ';
+            len=0;
+        }
+    }
+    cout<<len-1;//减去了句号
+    getchar();
+    getchar();
+    return 0;
+}
